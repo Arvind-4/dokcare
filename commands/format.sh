bc374f31e104cabca2b6a2ddc525f2ed50e77d55
Arvind-4 <lordarvindthegamer@gmail.com>
2023-10-01 20:56:41 +0530

    Updated: format command

--- commands/format.sh
+++ commands/format.sh
@@ -1,4 +1,10 @@
 #!bin/bash
 
-djlint . --reformat --format-css --format-js
-black . -v --diff
+echo "Formatting django templates ..."
+
+djlint . --reformat --format-js --indent-js 2 --format-css --indent-css 2 --indent 2 --use-gitignore --profile "django"
+
+echo "Formatting python files ..."
+
+black web manage.py app.py --check
+black web manage.py app.py
