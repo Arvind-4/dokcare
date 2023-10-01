107b8a2a75b4e8130298bd2bd08794d110b6efcc
Arvind-4 <lordarvindthegamer@gmail.com>
2023-10-01 15:49:06 +0530

    Updated: email config for the project

---
+++ web/backend/email/gmail.py
@@ -0,0 +1,10 @@
+from backend.env import config
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = config('EMAIL_PORT', cast=int, default=587)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER', cast=str)
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', cast=str)
+EMAIL_USE_SSL = True
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
